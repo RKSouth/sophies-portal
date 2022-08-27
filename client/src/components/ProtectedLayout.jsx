32b2aec6ec0e1ea1c9419e8dc4061e76475f0c74
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2022-08-27 13:50:33 -0700

    added some grid structure

--- client/src/components/ProtectedLayout.jsx
+++ client/src/components/ProtectedLayout.jsx
@@ -2,7 +2,6 @@ import { Link, Navigate, useOutlet } from "react-router-dom";
 import React, { useState } from 'react'
 import { useAuth } from "../utils/useAuth";
 import 'react-calendar/dist/Calendar.css';
-import Calendar from 'react-calendar';
 import MyCalender from "./calender";
 export const ProtectedLayout = () => {
 
@@ -14,12 +13,24 @@ export const ProtectedLayout = () => {
   if (!user) {
     return <Navigate to="/" />;
   } else {
-    return <div><h1>user profile</h1>
+    return <div>
     <h2>hello! {user.name}</h2>
+    <h3> Would you like to...</h3>
+    <div className="card">
     <button key={"logout"} onClick={logout}><h2>Logout</h2></button>
+    <button key={"logout"} onClick={logout}><h2>Schedule</h2></button>
+    <button key={"logout"} onClick={logout}><h2>Notes</h2></button>
+    <button key={"logout"} onClick={logout}><h2>Profile</h2></button>
+    </div>
+   
    
    <MyCalender/>
    
+
+   <div className="column">
+<div className="column50"></div>
+<div className="column50"></div>
+   </div>
     {/* {outlet}  */}
     </div>
   }
