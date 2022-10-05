d82caad86ccb866eb7cbcc888558c43030993d00
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2022-10-04 17:24:25 -0700

    added mapping to schedule

--- client/src/components/schedule.js
+++ client/src/components/schedule.js
@@ -1,36 +1,67 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { useAuth } from "../utils/useAuth";
-function Notes() {
+
+// const [ data, setShift ] = useState([]);
+
+const data = [
+    {
+        date: '08/24/90', day: 'thursday', start: '12:00AM', end: '12:00PM'
+    },
+    {
+        date: '08/24/23', day: 'monday', start: '12:00AM', end: '6:00PM'
+    }, {
+        date: '10/24/22', day: 'tuesday', start: '9:00AM', end: '12:00PM'
+    }, {
+        date: '11/24/96', day: 'saturday', start: '5:00AM', end: '2:00PM'
+    },
+]
+
+
+
+function addShift(e) {
+    // setShift([...data]);
+    console.log(data[1].date)
+
+}
+
+function submitShift(e) {
+
+}
+
+function Schedule() {
     const { user, logout } = useAuth();
     // const msg = useContext(UserContext)
     console.log(user, "from the home page");
-
+    console.log(data[0].date)
     return (
 
         <div className='container'>
 
-            <head className='card'>
+            <div className='card'>
                 <h1>My Schedule</h1>
-            </head>
+                <button onClick={addShift}> Add a Shift</button>
+                
+            </div>
             <main className='card'>
-                <table style={{textAlign:'left'}}>
+                <table style={{ textAlign: 'left', margin:'center' }}>
                     <tr>
                         <th>Date</th>
                         <th>Day</th>
                         <th>Start Time</th>
                         <th>End Time</th>
+                        <th>Delete</th>
                     </tr>
-                    <tr>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Maria Anders</td>
-                        <td>Germany</td>
-                    </tr>
-                    <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
-                    </tr>
+               
+                    {data.map(data =>
+                        <tr>
+                            <td>{data.date}</td>
+                            <td>{data.day}</td>
+                            <td>{data.start}</td>
+                            <td>{data.end}</td>
+                            <td><button>delete</button></td>
+                        </tr>
+                     
+                    )}
                 </table>
 
             </main>
@@ -40,4 +71,4 @@ function Notes() {
     );
 }
 
-export default Notes;
+export default Schedule;
